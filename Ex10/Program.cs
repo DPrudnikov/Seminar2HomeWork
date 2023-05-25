65c2d3a35fb0a3a6622997d7c3918a6c869cc34a
DPrudnikov <dima1911@mail.ru>
2023-05-25 20:17:22 +0300

    Add Ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456->5
+// 918->1
+
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+num = ((n % 100) - (n % 10)) / 10;
+Console.WriteLine(num);
+Console.WriteLine("End");
