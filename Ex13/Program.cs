806dd19332323cfa33a3b2df855bf8269622367a
DPrudnikov <dima1911@mail.ru>
2023-05-26 23:48:28 +0300

    add ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 6562679 -> 6
+
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (n > 1000)
+{
+    n = (n / 10);
+}
+Console.WriteLine(n % 10);
+}
+
+Console.WriteLine("End");
